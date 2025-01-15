64e4ebf1b8d0786feabdd7f50032fa5522610831
jobodd <38688162+jobodd@users.noreply.github.com>
2025-01-15 10:34:02 +0000

    Remove StatsLen func

--- internal/git/git.go
+++ internal/git/git.go
@@ -19,24 +19,17 @@ type GitOptions struct {
 }
 
 type GitStats struct {
-	CurrentBranch  string
-	HasRemote      bool
-	NCommitsAhead  int
-	NCommitsBehind int
-	NFilesAdded    int
-	NFilesRemoved  int
-	NFilesModified int
-	NFilesUnstaged int
-	ChangedFiles   []string
-}
-
-func (g *GitStats) StatsLen() int {
-	return len(strconv.Itoa(g.NCommitsAhead)) +
-		len(strconv.Itoa(g.NCommitsBehind)) +
-		len(strconv.Itoa(g.NFilesAdded)) +
-		len(strconv.Itoa(g.NFilesRemoved)) +
-		len(strconv.Itoa(g.NFilesModified)) +
-		len(strconv.Itoa(g.NFilesUnstaged))
+	CurrentBranch        string
+	HasRemote            bool
+	NCommitsAheadRemote  int
+	NCommitsBehindRemote int
+	NCommitsAheadBranch  int
+	NCommitsBehindBranch int
+	NFilesAdded          int
+	NFilesRemoved        int
+	NFilesModified       int
+	NFilesUnstaged       int
+	ChangedFiles         []string
 }
 
 func UpdateDirectory(absPath string, opts GitOptions) {
